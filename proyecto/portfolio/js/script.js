a29fa60893ae6e4238df8effeb26ca4ff413adb8
sunnivmr <sunnivmr@stud.ntnu.no>
2020-04-16 19:49:59 +0200

    #8 Sidenav works on about me

--- proyecto/portfolio/js/script.js
+++ proyecto/portfolio/js/script.js
@@ -1,5 +1,7 @@
 // Function for opening side nav
 function openNav() {
+
+    console.log("Opening sidenav");
     document.getElementsByClassName("sidenav")[0].style.width = "100%";
     document.getElementsByClassName("content")[0].style.marginLeft = "250px";
 }
@@ -7,6 +9,8 @@ function openNav() {
 
 // Function for closing side nav
 function closeNav() {
+
+    console.log("Closing sidenav");
     document.getElementsByClassName("sidenav")[0].style.width = "0";
     document.getElementsByClassName("content")[0].style.marginLeft = "0";
 }
