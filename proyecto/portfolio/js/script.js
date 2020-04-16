1594645fe765464baf489c843407b2b9eb4447ee
sunnivmr <sunnivmr@stud.ntnu.no>
2020-04-16 19:59:07 +0200

    Added functions for turning on and off dark mode

--- proyecto/portfolio/js/script.js
+++ proyecto/portfolio/js/script.js
@@ -14,3 +14,21 @@ function closeNav() {
     document.getElementsByClassName("sidenav")[0].style.width = "0";
     document.getElementsByClassName("content")[0].style.marginLeft = "0";
 }
+
+// Dark mode function
+function darkModeOn() {
+
+    console.log("Dark mode activated");
+
+
+    // Change background-color to dark 
+    document.getElementByTagName("body").style.backgroundColor("#202131");
+}
+
+function darkModeOff() {
+
+    console.log("Dark mode deactivated");
+
+    // Change background-color to light
+    document.getElementByTagName("body").style.backgroundColor("#B5B1E3");
+}
