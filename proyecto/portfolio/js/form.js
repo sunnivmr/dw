ba0b32b2d49cdd13bd3612ede391cf4f93635f96
sunnivmr <sunnivmr@stud.ntnu.no>
2020-05-01 20:06:08 +0200

    #13 #18 Opens popup with send-button

--- proyecto/portfolio/js/form.js
+++ proyecto/portfolio/js/form.js
@@ -1,7 +1,9 @@
 function sendMessage() {
     document.getElementById("popup").style.display = "block";
+    console.log("Message sent");
 }
 
 function closePopUp() {
     document.getElementById("popup").style.display = "none";
+    console.log("Closed popup");
 }
