f7110aa6e218d2d20d4a26cbb19d817742570795
sunnivmr <sunnivmr@stud.ntnu.no>
2020-05-01 19:39:20 +0200

    #13 #18 Added js-file and code for popup when sending message

---
+++ proyecto/portfolio/js/form.js
@@ -0,0 +1,7 @@
+function sendMessage() {
+    document.getElementById("popup").style.display = "block";
+}
+
+function closePopUp() {
+    document.getElementById("popup").style.display = "none";
+}
