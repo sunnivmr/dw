40a74cba76d99075e64697747812f60da0a3d869
sunnivmr <sunnivmr@stud.ntnu.no>
2020-05-02 13:15:44 +0200

    #17 Rewrite slideshow.js and import info portfolio.html

--- proyecto/portfolio/js/slideshow.js
+++ proyecto/portfolio/js/slideshow.js
@@ -1,31 +1,11 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slides = document.getElementById("slides");
 
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+// Prev slide
+function prevSlide() {
+    slides.style.marginLeft("-" + 100 + "%");
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slide");
-
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    slides[slideIndex - 1].style.display = "block";
+// Next slide
+function nextSlide() {
+    slides.style.marginLeft("+" + 100 + "%");
 }
