1efa091761b64eabba46ed50e2bb411b591a2de4
sunnivmr <sunnivmr@stud.ntnu.no>
2020-05-02 14:04:47 +0200

    #12 #17 Functioning image-slider

--- proyecto/portfolio/js/slideshow.js
+++ proyecto/portfolio/js/slideshow.js
@@ -1,16 +1,17 @@
-// Get slides-element
-function getSlides() {
-    return document.getElementById("slides");
-}
-
 // Prev slide
 function prevSlide() {
-    slides = getSlides();
-    slides.style.marginLeft = "-100%";
+    var slides = document.getElementById("slides");
+    var firstImage = document.getElementsByClassName("slide")[0];
+    var lastImage = document.getElementsByClassName("slide")[6];
+    slides.style.marginLeft = "-200%";
+    slides.insertAfter(lastImage, firstImage);
 }
 
 // Next slide
 function nextSlide() {
-    slides = getSlides();
-    slides.style.marginLeft = "100%";
+    var slides = document.getElementById("slides");
+    var firstImage = document.getElementsByClassName("slide")[0];
+    var lastImage = document.getElementsByClassName("slide")[6];
+    slides.style.marginLeft = 0;
+    slides.insertBefore(firstImage, lastImage);
 }
