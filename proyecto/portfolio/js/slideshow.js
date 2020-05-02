bc8eda3f7c48a3b21e76dfb9c1e497c1e9e9ba4e
sunnivmr <sunnivmr@stud.ntnu.no>
2020-05-02 14:22:51 +0200

    #12 #17 Next-button in slideshow works, but not the Prev-button

--- proyecto/portfolio/js/slideshow.js
+++ proyecto/portfolio/js/slideshow.js
@@ -1,17 +1,19 @@
 // Prev slide
 function prevSlide() {
     var slides = document.getElementById("slides");
-    var firstImage = document.getElementsByClassName("slide")[0];
-    var lastImage = document.getElementsByClassName("slide")[6];
-    slides.style.marginLeft = "-200%";
+    var images = document.getElementsByClassName("slide");
+    var firstImage = images[0];
+    var lastImage = images[images.length + 1];
+    slides.style.marginLeft = "-100%";
     slides.insertAfter(lastImage, firstImage);
 }
 
 // Next slide
 function nextSlide() {
     var slides = document.getElementById("slides");
-    var firstImage = document.getElementsByClassName("slide")[0];
-    var lastImage = document.getElementsByClassName("slide")[6];
+    var images = document.getElementsByClassName("slide");
+    var firstImage = images[0];
+    var lastImage = images[images.length];
     slides.style.marginLeft = 0;
     slides.insertBefore(firstImage, lastImage);
 }
